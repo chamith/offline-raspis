5ea582f959399084753064feb3a73b4ec2635895
Achila Liyanarachchi <achilal@ecollege.com>
2012-11-15 18:23:46 +0530

    Added full alphabet

--- services/cache_dictionary.php
+++ services/cache_dictionary.php
@@ -4,7 +4,7 @@ if(isset($_GET['filter'])) {
 
 }
 
-$alphabet = array( "a" );
+$alphabet = array( "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z");
 $data_dir = ".." . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "dictionary";
 
 if(!is_dir($data_dir)) {
