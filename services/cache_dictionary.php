27d2a67555e61eb59b51a1131b7dc90060940e9e
Achila Liyanarachchi <achilal@ecollege.com>
2012-11-15 15:42:56 +0530

    Renamed cache_data.php to cache_dictionary.php

--- services/cache_dictionary.php
+++ services/cache_dictionary.php
@@ -1,9 +1,10 @@
 <?php
 $alphabet = array( "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z");
-$data_dir = ".." . DIRECTORY_SEPARATOR . "data";
+$data_dir = ".." . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "dictionary";
 
 if(!is_dir($data_dir)) {
     @mkdir($data_dir);
+    chmod($data_dir , "0777");
 }
 
 foreach($alphabet as $char) {
