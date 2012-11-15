e5602d07c3a46c82cfea313fd2c14ffe0287d909
Fronter Virtual Machine <fronter@ubuntu.(none)>
2012-11-15 14:56:29 +0530

    Services to cache data

---
+++ services/cache_data.php
@@ -0,0 +1,31 @@
+<?php
+$alphabet = array( "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z");
+$data_dir = ".." . DIRECTORY_SEPARATOR . "data";
+
+foreach($alphabet as $char) {
+    $path = $data_dir . DIRECTORY_SEPARATOR . $char;
+    if(!is_dir($path)) {
+        @mkdir($path);
+	chmod($path , "0777");
+    }
+    
+    foreach($alphabet as $second_char) {
+        $full_char = $char . $second_char;
+        $path_second = $path . DIRECTORY_SEPARATOR . $full_char;
+        $file_path = $path_second . DIRECTORY_SEPARATOR . $full_char . ".xml";
+        
+        if(!is_dir($path_second)) {
+            @mkdir($path_second);
+	    chmod($path_second , "0777");
+        }
+        
+        $json_output = file_get_contents("http://api.pearson.com/longman/dictionary/entry.xml?q=" . $full_char . "&apikey=3452afbefdf2978f76ffa7bae5f694e5&jsonp=xml");
+        $file = fopen($file_path , "w");
+        fwrite($file , $json_output);
+	chmod($file , "0777");
+        fclose($file);
+    }
+echo "Done";
+}
+//$json_output = file_get_contents("http://api.pearson.com/longman/dictionary/entry.xml?q=*&apikey=3452afbefdf2978f76ffa7bae5f694e5&jsonp=xml");
+?>
